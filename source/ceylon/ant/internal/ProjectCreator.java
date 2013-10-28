1603601b0f6fc47c7e048c706595429013af535f
HenningB <henning@lpphysics.com>
2013-10-28 21:15:31 +0100

    BytesURLConnection and BytesURLStreamHandler now part of ProjectCreator

--- source/ceylon/ant/internal/ProjectCreator.java
+++ source/ceylon/ant/internal/ProjectCreator.java
@@ -1,7 +1,11 @@
 package ceylon.ant.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
 import java.util.Vector;
 
 import org.apache.tools.ant.BuildLogger;
@@ -13,6 +17,29 @@ import org.apache.tools.ant.types.resources.StringResource;
 
 public class ProjectCreator {
 
+    public static class BytesURLStreamHandler extends URLStreamHandler {
+        private byte[] content;
+        public BytesURLStreamHandler(byte[] content) {
+            this.content = content;
+        }
+        public URLConnection openConnection(URL url) {
+            return new BytesURLConnection(url, content);
+        }
+    }
+    
+    public static class BytesURLConnection extends URLConnection {
+        protected byte[] content;
+        public BytesURLConnection(URL url, byte[] content) {
+            super(url);
+            this.content = content;
+        }
+        public void connect() {
+        }
+        public InputStream getInputStream() {
+            return new ByteArrayInputStream(content);
+        }
+    }
+    
     private final static String MINIMAL_BUILD_FILE = "<project default=\"main\" basedir=\".\"><target name=\"main\"><echo message=\"Hello world!\"/></target></project>";
 
     public static Project createProject() {
@@ -22,10 +49,10 @@ public class ProjectCreator {
             project.fireBuildStarted();
             // do what ProjectHelper.configureProject(project, new File("build.xml")); does
             StringResource stringResource = new StringResource(project, MINIMAL_BUILD_FILE);
-            URL url = new URL( null, "byte:bytes", new BytesURLStreamHandler(MINIMAL_BUILD_FILE.getBytes()));
-            ProjectHelper helper = ProjectHelperRepository.getInstance().getProjectHelperForBuildFile(stringResource);
-            project.addReference(ProjectHelper.PROJECTHELPER_REFERENCE, helper);
-            helper.parse(project, url);
+            URL url = new URL(null, "byte:bytes", new BytesURLStreamHandler(MINIMAL_BUILD_FILE.getBytes()));
+            ProjectHelper projectHelper = ProjectHelperRepository.getInstance().getProjectHelperForBuildFile(stringResource);
+            project.addReference(ProjectHelper.PROJECTHELPER_REFERENCE, projectHelper);
+            projectHelper.parse(project, url);
             // execute main with echo task, so that tasks get known to Ant
             Vector<String> targets = new Vector<>();
             targets.add("main");
